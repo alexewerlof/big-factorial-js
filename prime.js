f048dce8e03f90563c13eaddb8b40ac9cb758462
Alex Ewerlöf <hanifbbz@gmail.com>
2019-11-02 23:15:05 +0100

    some optimization

--- prime.js
+++ prime.js
@@ -1,6 +1,10 @@
 function getFirstPrimeFactor(x, primes) {
-	const limit = Math.sqrt(x)
-    return primes.find(p => (p <= limit) && (x % p === 0)) || x
+	// If it is even do this, otherwise just divide by 2 and don't bother computing sqrt and linerar array search
+	if (x & 1) {
+		const limit = Math.sqrt(x)
+		return primes.find(p => (p <= limit) && (x % p === 0)) || x
+	}
+	return 2
 }
 
 function _digest(n, primes) {
@@ -20,21 +24,23 @@ function _digest(n, primes) {
 function digestAll(n) {
 	const primes = []
 	const pows = {}
+	const sqrtN = Math.sqrt(n)
 	for (let i = 2; i <= n; i++) {
 		let res = _digest(i, primes)
-		if (res.length === 1) {
-			primes.push(res[0])
-			// console.log("Oh a new prime:", res, primes)
+		let res0 = res[0]
+		if (res.length === 1 && res0 <= sqrtN) {
+			primes.push(res0)
+			//console.log("Oh a new prime:", res0)
 		}
 		res.forEach(rInt => {
 			if (rInt in pows) {
-				pows[rInt]++
-			} else {
-				pows[rInt] = 1
+				return pows[rInt]++
 			}
+			pows[res0] = 1
 		})
-		// console.log("digest", i, res)
+		//console.log("digest", i, res)
 	}
+	console.dir(pows)
 	return pows
 }
 
